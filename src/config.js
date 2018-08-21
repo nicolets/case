a6647f0fbc013c443c832030582b329709c9519c
bjornstromeqt <bjornstrom90@gmail.com>
2018-08-21 17:09:47 +0200

    Add comments to config-file

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 
+// Replace token
+const TOKEN = 'INSERT_TOKEN';
 
-export const CONFIG = {
-    GRAPHQL_URL: 'https://alva-backend-dev.appspot.com/graphql',
-    TOKEN: 'INSERT_TOKEN',
-    ORGANIZATION_ID: 'T3JnYW5pemF0aW9uOjc='
-};
+// URL to our development environment.
+const GRAPHQL_URL = 'https://alva-backend-dev.appspot.com/graphql';
+
+// The ID for the organization for which you will query data.
+const ORGANIZATION_ID = 'T3JnYW5pemF0aW9uOjc=';
+
+export const CONFIG = {TOKEN, GRAPHQL_URL, ORGANIZATION_ID};
