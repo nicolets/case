9f1d7e34a494cac5698f557e9b0fe6415e622170
nicolets <43137019+nicolets@users.noreply.github.com>
2019-02-05 21:00:06 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 
 // Replace token
-const TOKEN = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTU0OTM4OTkxOSwiZXhwIjoxNTQ5OTk0NzE5fQ.eyJpZCI6MzE2fQ.G9P54q0E9NRADRGS-9sQXxE9thjMSxbWZ-WyOpka22kRDeoamFSZDZrtzZyEQ4ysBIW7u29Cdey_P-IiR2Wl_A
-';
+const TOKEN = 'eyJhbGciOiJIUzUxMiIsImlhdCI6MTU0OTM4OTkxOSwiZXhwIjoxNTQ5OTk0NzE5fQ.eyJpZCI6MzE2fQ.G9P54q0E9NRADRGS-9sQXxE9thjMSxbWZ-WyOpka22kRDeoamFSZDZrtzZyEQ4ysBIW7u29Cdey_P-IiR2Wl_A';
 
 // URL to our development environment.
 const GRAPHQL_URL = 'https://alva-backend-dev.appspot.com/graphql';
