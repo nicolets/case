b1f1a3e71ba620192be6040b3d8d36949889f3b6
nicolets <43137019+nicolets@users.noreply.github.com>
2019-02-08 00:02:41 +0100

    Update index.js
    changes to get results from queries for user firstname,lastname,logictestscore and teamname

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,14 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import {WithOrganizations} from '../../containers/WithOrganization';
 import {H1} from '../../components/H1';
-
 import {CONFIG} from '../../config';
 
 
+
+
 class Home extends React.Component {
+//creating a table to iterate through data.organization.employments.edges, the array that contains the employees
+    createTable = (mydata) => {
+        let table = []
+        for (let i = 0; i < mydata.edges.length; ++i ) {
+          let children = []
+          //accessing first and last name fields of (node) user
+           children.push(<td>{`First Name: ${ mydata.edges[i].node.user.firstName}`}</td>)
+          children.push(<td>{`Last Name: ${ mydata.edges[i].node.user.lastName}`}</td>)
+          //some entries don't have either logic test score or team for the user so we make sure we dont stumble across them
+          //could be letter improved with a try-catch maybe
+          //it could also be done for the fields lastname and first name
+          if (mydata.edges[i].node.user.employments.edges[0].node.teams.edges[0]!=null){
+            children.push(<td>{`Team: ${ mydata.edges[i].node.user.employments.edges[0].node.teams.edges[0].node.team.name}`}</td>)
+          }
+          else{
+            children.push(<td>{`Team:-`}</td>)
+          }
+          if (mydata.edges[i].node.user.logicTest.edges[0]!=null){
+            children.push(<td>{`Logic Test Score: ${ mydata.edges[i].node.user.logicTest.edges[0].node.score}`  }</td>)
+        }
+          else{
+            children.push(<td>{`Logic Test Score:-`}</td>)
+          }
+          table.push(<tr>{children}</tr>)
+        }
+        return table
+      };
+        
 
     renderError = (error) => {
-        console.log(error);
+        console.log(error)
 
         return (
             <div>
@@ -29,11 +58,11 @@ class Home extends React.Component {
                             return <p>Loading</p>;
                         }
 
+                
                         return (
-                            <div>
-                                <p>ID: {data.organization.id}</p>
-                                <p>Name: {data.organization.name}</p>
-                            </div>
+                            <table>
+                            {this.createTable(data.organization.employments)}
+                            </table>
                         );
                     }}
                 </WithOrganizations>
@@ -46,5 +75,6 @@ class Home extends React.Component {
 const Wrapper = styled.div`
    padding: 24px;
 `;
+
 
 export default Home;
