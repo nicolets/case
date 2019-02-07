16b0451125f03860b6c408d54472deb3f19e1f6e
nicolets <43137019+nicolets@users.noreply.github.com>
2019-02-07 23:59:27 +0100

    Update index.js
    changes to get results from queries for user firstname,lastname,logictestscore and teamname

--- src/containers/WithOrganization/index.js
+++ src/containers/WithOrganization/index.js
@@ -1,34 +1,80 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
-
-export const GET_ORGANIZATIONS = gql`
-query getOrganization($id: ID!) {
-  organization(id: $id) {
-    id
-    name    
-  }
-}
-`;
+import styled from 'styled-components';
+import {WithOrganizations} from '../../containers/WithOrganization';
+import {H1} from '../../components/H1';
+import {CONFIG} from '../../config';
 
 
-export class WithOrganizations extends React.Component {
-    static propTypes = {
-        children: PropTypes.func.isRequired,
-        onError: PropTypes.func.isRequired,
-        organizationId: PropTypes.string.isRequired
+
+
+class Home extends React.Component {
+//creating a table to iterate through data.organization.employments.edges, the array that contains the employees
+    createTable = (mydata) => {
+        let table = []
+        for (let i = 0; i < mydata.edges.length; ++i ) {
+          let children = []
+          //accessing first and last name fields of (node) user
+           children.push(<td>{`First Name: ${ mydata.edges[i].node.user.firstName}`}</td>)
+          children.push(<td>{`Last Name: ${ mydata.edges[i].node.user.lastName}`}</td>)
+          //some entries don't have either logic test score or team for the user so we make sure we dont stumble across them
+          //could be letter improved with a try-catch maybe
+          //it could also be done for the fields lastname and first name
+          if (mydata.edges[i].node.user.employments.edges[0].node.teams.edges[0]!=null){
+            children.push(<td>{`Team: ${ mydata.edges[i].node.user.employments.edges[0].node.teams.edges[0].node.team.name}`}</td>)
+          }
+          else{
+            children.push(<td>{`Team:-`}</td>)
+          }
+          if (mydata.edges[i].node.user.logicTest.edges[0]!=null){
+            children.push(<td>{`Logic Test Score: ${ mydata.edges[i].node.user.logicTest.edges[0].node.score}`  }</td>)
+        }
+          else{
+            children.push(<td>{`Logic Test Score:-`}</td>)
+          }
+          table.push(<tr>{children}</tr>)
+        }
+        return table
+      };
+        
+
+    renderError = (error) => {
+        console.log(error)
+
+        return (
+            <div>
+                <h1>Something went wrong</h1>
+            </div>
+        );
     };
 
     render() {
-        const variables = {id: this.props.organizationId};
 
         return (
-            <Query query={GET_ORGANIZATIONS} notifyOnNetworkStatusChange={true} variables={variables}>
-                {({data, loading, error}) => error ? this.props.onError(error) : this.props.children({loading, data})}
-            </Query>
+            <Wrapper>
+                <H1>Home</H1>
+                <WithOrganizations onError={this.renderError} organizationId={CONFIG.ORGANIZATION_ID}>
+                    {({loading, data}) => {
+                        if (loading) {
+                            return <p>Loading</p>;
+                        }
+
+                
+                        return (
+                            <table>
+                            {this.createTable(data.organization.employments)}
+                            </table>
+                        );
+                    }}
+                </WithOrganizations>
+            </Wrapper>
         );
     }
 }
 
-export default WithOrganizations;
+
+const Wrapper = styled.div`
+   padding: 24px;
+`;
+
+
+export default Home;
